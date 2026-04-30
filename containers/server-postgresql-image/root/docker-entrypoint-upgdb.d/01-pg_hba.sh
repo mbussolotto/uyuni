079129268931e16df5a6ab88caa113569483c6c0
Ondrej Holecek <oholecek@suse.com>
2026-04-30 09:54:48 +0200

    Use sed instead of grep in pgsql functions as grep is not available everywhere

--- containers/server-postgresql-image/root/docker-entrypoint-upgdb.d/01-pg_hba.sh
+++ containers/server-postgresql-image/root/docker-entrypoint-upgdb.d/01-pg_hba.sh
@@ -7,14 +7,14 @@
 
 HBA_FILE=/var/lib/pgsql/data/pg_hba.conf
 
-if ! grep -Fqx "local all all peer" "$HBA_FILE"; then
+if [ -z "$(sed -n '/^local all all peer$/p' "$HBA_FILE")" ]; then
     echo "local all all peer" >> "$HBA_FILE"
 fi
 
-if ! grep -Fqx "local replication all peer" "$HBA_FILE"; then
+if [ -z "$(sed -n '/^local replication all peer$/p' "$HBA_FILE")" ]; then
     echo "local replication all peer" >> "$HBA_FILE"
 fi
 
-if ! grep -Fqx "host all all all scram-sha-256" "$HBA_FILE"; then
+if [ -z "$(sed -n '/^host all all all scram-sha-256$/p' "$HBA_FILE")" ]; then
     echo "host all all all scram-sha-256" >> "$HBA_FILE"
 fi
